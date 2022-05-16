ab86ccda4af83442af59caf843496d75aa9a95ef
ResamZaha <resam.zaha@gmail.com>
2022-05-16 19:03:01 +0600

    second commit in bran1

---
+++ f2.java
@@ -0,0 +1,19 @@
+class Abc{
+    int a;
+    int b;
+    public void set_a(int x)
+    { a=x; }
+    public void set_b(int y){
+        b=y;
+    }
+    public int get_a(){
+        return a;
+    }
+    public int get_b(){
+        return b;
+    }
+    Abc(int xx, int yy){
+    x = xx;
+    y = yy;
+}
+};
